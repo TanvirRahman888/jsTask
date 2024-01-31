929299b3f8f3a55827b97224b5406a0b64fb59f0
Tanvir Module 1 <tanvirrahman8888@gmail.com>
2024-02-01 02:43:32 +0600

    while update

--- js-loop-tasks/commitment.js
+++ js-loop-tasks/commitment.js
@@ -3,3 +3,5 @@ while(i<=60){
     console.log("I will invest at least 6 hrs every single day for next 60 days!-- ",i);
     i++;
 }
+
+    
--- js-loop-tasks/multiplication table.js
+++ js-loop-tasks/multiplication table.js
@@ -1,6 +1,12 @@
-const num =5;
+// const num =5;
 // let multiplication =[];
-for (let i = 1; i <= 10; i++) {
-    console.log("5 x ",i,"= ",5*i);
+// for (let i = 1; i <= 10; i++) {
+//     console.log("5 x ",i,"= ",5*i);
     
+// }
+let n=1;
+let num=5;
+while(n<=10){
+    console.log("5 x ",n,"= ",5*n);
+    n++;
 }
