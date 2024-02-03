9b735679cac1bb744e285307f1188d82f8ee64bd
Tanvir Module 1 <tanvirrahman8888@gmail.com>
2024-02-04 04:48:52 +0600

    Function Task 5 is complete

--- js-function-practice-tasks/Task-5.js
+++ js-function-practice-tasks/Task-5.js
@@ -1,10 +1,10 @@
 //Write a function called odd_even() which takes an integer value and tells whether this value is even or odd. If even return Even. If odd return Odd
 function odd_even(value) {
     if (value % 2 == 0) {
-        return "Even Number";
+        return "Even Number.";
 
     } else {
-        return "Odd Number";
+        return "Odd Number.";
 
     }
 }
